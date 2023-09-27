c3fbc8f46a605273e7f57133adda606177ddaa32
webbjulia <juliacw@unc.edu>
2023-09-26 22:14:38 -0400

    Removes intermediate file

--- compare.sh
+++ compare.sh
@@ -2,10 +2,10 @@ wc_man=$(man man | wc -l)
 wc_ls=$(man ls | wc -l)
 wc_find=$(man find | wc -l)
 
-rm result.sh
-
 echo "man, "$wc_man" lines" >> result.sh
 echo "ls, "$wc_ls" lines" >> result.sh
 echo "find, "$wc_find" lines" >> result.sh
 
 cat result.sh | sort -g -k 2 -r -t ,
+
+rm result.sh
